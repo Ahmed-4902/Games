6c0c9c07f2902dfe8e9487bee4bbde9e0e146098
Ahmed <teleb0.1am@gmail.com>
2023-01-29 20:25:08 +0200

    funny sound

--- 02-memory/main.js
+++ 02-memory/main.js
@@ -1,15 +1,15 @@
 document.querySelector(".control-buttons span").onclick = function () {
-   let yourName = prompt("What Your Name? ");
+	let yourName = prompt("What Your Name? ");
 
-   if (yourName == null || yourName == "") {
-      document.querySelector(".info-container .name span").innerHTML =
-         "Unknown";
-   } else {
-      document.querySelector(
-         ".info-container .name"
-      ).innerHTML = `Hello: ${yourName}`;
-   }
-   document.querySelector(".control-buttons").remove();
+	if (yourName == null || yourName == "") {
+		document.querySelector(".info-container .name span").innerHTML =
+			"Unknown";
+	} else {
+		document.querySelector(
+			".info-container .name"
+		).innerHTML = `Hello: ${yourName}`;
+	}
+	document.querySelector(".control-buttons").remove();
 };
 
 let duration = 1000;
@@ -20,65 +20,65 @@ let orderRange = [...Array(blocks.length).keys()];
 shuffle(orderRange);
 
 blocks.forEach((block, index) => {
-   block.style.order = orderRange[index];
-   block.addEventListener("click", function () {
-      flipBlock(block);
-   });
+	block.style.order = orderRange[index];
+	block.addEventListener("click", function () {
+		flipBlock(block);
+	});
 });
 
 // Flip block Function
 function flipBlock(slectedBlock) {
-   // Add Class is-flipped
-   slectedBlock.classList.add("is-flipped");
+	// Add Class is-flipped
+	slectedBlock.classList.add("is-flipped");
 
-   // Collect all flipped card
-   let allFlippedBlocks = blocks.filter((flippedblock) =>
-      flippedblock.classList.contains("is-flipped")
-   );
+	// Collect all flipped card
+	let allFlippedBlocks = blocks.filter((flippedblock) =>
+		flippedblock.classList.contains("is-flipped")
+	);
 
-   // If There two selected blocks
-   if (allFlippedBlocks.length === 2) {
-      stopClicking();
-      checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
-   }
+	// If There two selected blocks
+	if (allFlippedBlocks.length === 2) {
+		stopClicking();
+		checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
+	}
 }
 
 // STop Clicking function
 function stopClicking() {
-   // Add Class No Clicking on Main Container
-   blocksContainer.classList.add("no-clicking");
+	// Add Class No Clicking on Main Container
+	blocksContainer.classList.add("no-clicking");
 
-   // Remove Class No Clicking on Main Container
-   setTimeout(() => {
-      blocksContainer.classList.remove("no-clicking");
-   }, duration);
+	// Remove Class No Clicking on Main Container
+	setTimeout(() => {
+		blocksContainer.classList.remove("no-clicking");
+	}, duration);
 }
 
 //Check Matched Block
 function checkMatchedBlocks(firstBlock, secondBlock) {
-   let triesElement = document.querySelector(".tries span");
+	let triesElement = document.querySelector(".tries span");
 
-   if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
-      firstBlock.classList.remove("is-flipped");
-      secondBlock.classList.remove("is-flipped");
+	if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+		firstBlock.classList.remove("is-flipped");
+		secondBlock.classList.remove("is-flipped");
 
-      firstBlock.classList.add("has-match");
-      secondBlock.classList.add("has-match");
-      document.getElementById("success").play();
-   } else {
-      triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
-      setTimeout(() => {
-         firstBlock.classList.remove("is-flipped");
-         secondBlock.classList.remove("is-flipped");
-      }, duration);
-      document.getElementById("fail").play();
-   }
+		firstBlock.classList.add("has-match");
+		secondBlock.classList.add("has-match");
+		document.getElementById("success").play();
+	} else {
+		triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
+		setTimeout(() => {
+			firstBlock.classList.remove("is-flipped");
+			secondBlock.classList.remove("is-flipped");
+		}, duration);
+		document.getElementById("fail2").play();
+	}
 }
 
 // Shuffle function
 function shuffle(array) {
-   for (let i = array.length - 1; i > 0; i--) {
-      let random = Math.floor(Math.random() * i);
-      [array[i], array[random]] = [array[random], array[i]];
-   }
+	for (let i = array.length - 1; i > 0; i--) {
+		let random = Math.floor(Math.random() * i);
+		[array[i], array[random]] = [array[random], array[i]];
+	}
 }
